ef69899ef2a7d9d1d2c46d8463c040766486e801
Fahim <fahimakhtar928@gmail.com>
2025-06-21 18:08:58 +0100

    Update MyFirstC#.cs
    Adding a couple changes

--- MyFirstC#.cs
+++ MyFirstC#.cs
@@ -1,6 +1,6 @@
 using System;  // using System means that we can use classes from the System namespace.
 
-namespace MyApplication // namespace is used to organize your code, and it is a container for classes and other namespaces.
+namespace MyRepository // namespace is used to organize your code, and it is a container for classes and other namespaces.
 {
   class Program // class is a container for data and methods, which brings functionality to your program. Every line of code that runs in C# must be inside a class. In our example, we named the class Program.
   {
