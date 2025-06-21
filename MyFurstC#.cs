51e89dec8b86de5bfac539c82f4d7150169ed9ca
Fahim <fahimakhtar928@gmail.com>
2025-06-21 18:05:24 +0100

    Update MyFirstC#.cs
    Grammatical error fix and adding notes to ease confusion.

--- MyFurstC#.cs
+++ MyFurstC#.cs
@@ -1,14 +1,14 @@
-using System;
+using System;  // using System means that we can use classes from the System namespace.
 
-namespace MyApplication
+namespace MyApplication // namespace is used to organize your code, and it is a container for classes and other namespaces.
 {
-  class Program
+  class Program // class is a container for data and methods, which brings functionality to your program. Every line of code that runs in C# must be inside a class. In our example, we named the class Program.
   {
-    static void Main(string[] args)
+    static void Main(string[] args)  
     {
-      Console.WriteLine("Hello World!");
-      Console.WriteLine("I am Learning C#");
-      Console.WriteLine("It is awesome!");
+      Console.WriteLine("Hello World!");     |
+      Console.WriteLine("I am Learning C#"); | // All of these are what's gonna be logged on the console.
+      Console.WriteLine("It is awesome!");   |
     }
   }
 }
